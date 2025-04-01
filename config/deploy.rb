794b86712dd90ca41cd8e9fb6112e366fc4a75c7
nitz8867 <nitish.kokari@gmail.com>
2025-04-01 16:54:36 +0530

    Configure Puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,3 +45,17 @@ set :branch, 'master'  # Set the branch name you want to deploy
 set :default_env, {
   'SECRET_KEY_BASE' => '1dee92d80aeea1cb31a431dead41f470540484f1659e299b6ebbefaa4afb5dceeaa94417e17c8c2ea07b0f3b8b55bec9b30394e3c61b1530b0b64cad208320ed'
 }
+
+# Puma configuration
+
+set :puma_threads, [4, 16]
+set :puma_workers, 2
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log, "#{release_path}/log/puma.error.log"
+set :puma_preload_app, true
+set :puma_daemonize, false
+set :puma_prune_bundler, true
+
