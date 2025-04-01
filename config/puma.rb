d0a0dbf135721c651bbb4e647ffb3a7062dc8572
nitz8867 <nitish.kokari@gmail.com>
2025-04-01 18:42:37 +0530

    add tailwind

--- config/puma.rb
+++ config/puma.rb
@@ -39,7 +39,7 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 
 threads min_threads_count, max_threads_count
 port ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "production" }
+environment ENV.fetch("RAILS_ENV") { "development" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/puma.pid" }
 if Rails.env.development?
   # Disable worker processes for development (only threads are used)
